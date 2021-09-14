e18ca86c23b72ebe2c3a6f85383ff937f682f0de
Jose Mora <josmo@microsoft.com>
2021-09-14 16:56:37 -0500

    first commit of hangman

---
+++ Hangman/Hangman/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hangman!");
+            Console.WriteLine();
+
+            string[] wordList = { "apple", "grape", "orange", "strawberry", "pear" };
+
+            Random random = new Random();
+            int wordListInt = random.Next(0, wordList.Length - 1);
+
+            string chosenWord = wordList[wordListInt].ToLower();
+            Console.WriteLine("The word has {0} characters", chosenWord.Length);
+
+            int remainingAttempts = 7;
+            List<string> alreadyGuessedLetters = new List<string>();
+            string displayWord = "";
+
+            while (remainingAttempts > 0 && displayWord != chosenWord)
+            {
+                Console.WriteLine("Remaining attempts: {0}", remainingAttempts);
+
+                Console.WriteLine("Enter a letter: ");
+                char[] input = Console.ReadLine().ToCharArray();
+
+                string guess = "";
+                if (input.Length > 0)
+                {
+                    guess = input[0].ToString();
+                    guess = guess.ToLower();
+                }
+
+                alreadyGuessedLetters.Add(guess);
+
+                displayWord = "";
+                string containedMessage = "";
+                if (chosenWord.Contains(guess))
+                {
+                    containedMessage = "{0} is contained in the word.";
+                }
+                else
+                {
+                    containedMessage = "{0} is NOT contained in the word.";
+                    remainingAttempts--;
+                }
+
+                Console.WriteLine(containedMessage, guess);
+
+                foreach (var letter in chosenWord)
+                {
+                    if (alreadyGuessedLetters.Contains(letter.ToString()))
+                    {
+                        displayWord += letter;
+                    }
+                    else
+                    {
+                        displayWord += "*";
+                    }
+                }
+
+                Console.WriteLine(displayWord);
+                Console.WriteLine();
+            } 
+
+            if (remainingAttempts == 0)
+            {
+                Console.WriteLine("You have ran out of attempts. You lose!");
+                Console.WriteLine("The word was: {0}", chosenWord);
+            }
+            else
+            {
+                Console.WriteLine("You have won the game!");
+            }
+        }
+    }
+}
